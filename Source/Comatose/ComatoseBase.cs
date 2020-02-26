275a71bfc584e98e963de55c5b7d08ca9e576c71
Zach Hill <zach.adam.hill@gmail.com>
2020-02-25 22:52:57 -0500

    Update to 1.1

---
+++ Source/Comatose/ComatoseBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HugsLib;
+
+namespace Comatose
+{
+    public class ComatoseBase : ModBase
+    {
+        public override string ModIdentifier
+        {
+            get
+            {
+                return "Comatose";
+            }
+        }
+    }
+}
---
+++ Source/Comatose/HediffDefOfDeathRattleStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+
+namespace Comatose
+{
+    public static class HediffDefOfDeathRattleStrings
+    {
+        public static string IntestinalFailure = "IntestinalFailure";
+        public static string LiverFailure = "LiverFailure";
+        public static string KidneyFailure = "KidneyFailure";
+        public static string ClinicalDeathNoHeartbeat = "ClinicalDeathNoHeartbeat";
+        public static string ClinicalDeathAsphyxiation = "ClinicalDeathAsphyxiation";
+        public static string Coma = "Coma";
+    }
+}
+
